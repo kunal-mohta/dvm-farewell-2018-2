1494a48a4254c274d1d7a61badf19895b631a7ac
kunal-mohta <kunalmohta1818@gmail.com>
2018-04-14 08:30:24 +0530

    bg etc

--- components.js
+++ components.js
@@ -11,62 +11,62 @@ class Wrapper extends React.Component {
         {
           name:'Prateek Gupta',
           team:f,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Shivani Mittal',
           team:a,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Jai Agarwal',
           team:a,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Arjun Tyagi',
           team:v,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Vihang Shah',
           team:d,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Vishal Hans',
           team:d,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Amey Agarwal',
           team:d,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Satwik Bhattamishra',
           team:b,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Kalpit Munot',
           team:v,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Kunal Sharma',
           team:b,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Piyush Ranjan',
           team:f,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         },
         {
           name:'Nitish Kundala',
           team:a,
-          id:'20xxxxxxx'
+          id:'2017xxpsxxxxp'
         }
       ],
       venue:'Kamal',
@@ -191,47 +191,54 @@ ReactDOM.render(
   <Wrapper />,
   document.getElementById('wrapper')
 )
-var containers = document.getElementsByClassName('personContainer');
-var pics = document.getElementsByClassName('pic');
-var teams = document.getElementsByClassName('team1');
-var teams2 = document.getElementsByClassName('team2');
-var names = document.getElementsByClassName('name');
-var bars = document.getElementsByClassName('bar');
 
-var container = document.getElementsByClassName('personContainer')[0];
-var pic = document.getElementsByClassName('pic')[0];
-var team1 = document.getElementsByClassName('team1')[0];
+$(document).ready(function(){
+  var containers = document.getElementsByClassName('personContainer');
+  var pics = document.getElementsByClassName('pic');
+  var teams = document.getElementsByClassName('team1');
+  var teams2 = document.getElementsByClassName('team2');
+  var names = document.getElementsByClassName('name');
+  var bars = document.getElementsByClassName('bar');
 
-pic.style.height = (container.clientHeight < container.clientWidth ? container.clientHeight : container.clientWidth)*0.7 + "px";
+  var container = document.getElementsByClassName('personContainer')[0];
+  var pic = document.getElementsByClassName('pic')[0];
+  var team1 = document.getElementsByClassName('team1')[0];
 
-team1.style.top = (container.clientHeight + pic.clientHeight)/2 + "px";
-team1.style.transform = "translateY(-50%)";
+  pic.style.height = (container.clientHeight < container.clientWidth ? container.clientHeight : container.clientWidth)*0.7 + "px";
 
-for(var i=1; i<pics.length; i++)
-{
-  pics[i].style.height = pics[0].style.height;
-  teams[i].style.top  = teams[0].style.top;
-  teams[i].style.transform = teams[0].style.transform;
-  pics[i].style.opacity = 0;
-  teams[i].style.opacity = 0;
-  teams2[i].style.opacity = 0;
-  names[i].style.opacity = 0;
-  bars[i].style.width = 0;
-}
+  team1.style.top = (container.clientHeight + pic.clientHeight)/2 + "px";
+  team1.style.transform = "translateY(-50%)";
 
-document.addEventListener("keyup", toggle);
-
-var active = 0;
-
-function toggle(e)
-{
-  var length = containers.length;
-  if(e.keyCode == 39)
+  for(var i=1; i<pics.length; i++)
   {
-    pics[active].style.opacity = 0;
-    setTimeout(function(){pics[active+1].style.opacity = 1;}, 1100);
-    bars[active].style.width = 0;
-    setTimeout(function(){bars[++active].style.width = "60%";}, 1200);
-    // teams[active].style.top = ;
+    pics[i].style.height = pics[0].style.height;
+    teams[i].style.top  = teams[0].style.top;
+    teams[i].style.transform = teams[0].style.transform;
+    pics[i].style.opacity = 0;
+    teams[i].style.opacity = 0;
+    teams2[i].style.opacity = 0;
+    names[i].style.opacity = 0;
+    bars[i].style.width = 0;
   }
-}
+
+  document.addEventListener("keyup", toggle);
+
+  var active = 0;
+
+  function toggle(e)
+  {
+    var length = containers.length;
+    if(e.keyCode == 39)
+    {
+      pics[active].style.opacity = 0;
+      setTimeout(function(){pics[active+1].style.opacity = 1;}, 1100);
+      bars[active].style.width = 0;
+      setTimeout(function(){bars[++active].style.width = "60%";}, 1200);
+      // teams[active].style.top = ;
+
+      $('.name:eq(0), .team1:eq(0), .team2:eq(0), .idno:eq(0)').animate({
+        width:'toggle'
+      });
+    }
+  }
+});
